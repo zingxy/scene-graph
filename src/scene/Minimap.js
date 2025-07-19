348982ded7d511ed79e8ca1884c35cea41b22983
hongqing <zenghongqing@iplusmobot.com>
2025-07-19 16:00:59 +0800

    feat: 提取出Minimap逻辑

---
+++ src/scene/Minimap.js
@@ -0,0 +1,27 @@
+import { Camera } from './Camera';
+import { Rect } from './DisplayObject';
+import { SceneGraph } from './World';
+class Minimap {
+  constructor(mainWorld, canvas) {
+    const minimapWorld = new SceneGraph(canvas);
+    this.miniWorld = minimapWorld;
+    this.mainWorld = mainWorld;
+    this.miniWorld.stage.addChild(this.mainWorld.stage);
+    this.cameraRect = this.miniWorld.stage.addChild(
+      new Rect(canvas.width, canvas.height)
+    );
+    this.update();
+  }
+  update() {
+    this.cameraRect.transformMatrix =
+      this.mainWorld.camera.transformMatrix.inverse();
+    requestAnimationFrame(() => {
+      this.update();
+    });
+  }
+  render() {
+    this.miniWorld.render();
+  }
+}
+
+export { Minimap };
